88e0d84448ee47e04770564671610a9fcb9c95fe
Marcelo Sanchez <yinoyino@gmail.com>
2020-01-29 11:12:30 -0300

    Form validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(params.require(:user).permit(:username, :password))
-    session[:user_id] = @user.id
-    redirect_to '/welcome'
+    @user = User.new(params.require(:user).permit(:username, :email, :password, :password_confirmation))
+    if @user.save()
+      session[:user_id] = @user.id
+      redirect_to '/welcome'
+    else  
+      render 'new' and return
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@ class User < ApplicationRecord
 
     has_secure_password
 
+    validates :username, presence: { message: "no puede ser vacio"}
+    validates :username, uniqueness: { message: "ya está en uso" }
+    validates :password, presence: { message: "no puede ser vacio" }
+    validates :email, presence: { message: "no puede ser vacio" }
+    validates :password, confirmation: { case_sensitive: true, message: "Las contraseñas son distintas" }
+    validate :must_be_email
+
+    def must_be_email
+        unless email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+            errors.add(:email, "no es un email")
+        end
+    end
 end
